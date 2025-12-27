53ed7b9f2bd54bdc2ef115a3436a0531a422cf9d
Artyom Palchun <artyompalchun95@gmail.com>
2025-12-27 14:39:24 +0300

    Задание 2.7

---
+++ 7lab/js/script.js
@@ -0,0 +1,140 @@
+const skills = {
+  data: [],
+
+  sortConfig: {
+    type: null,   
+    order: 'asc'  
+  },
+
+  generateList(parentElement) {
+    parentElement.innerHTML = '';
+    this.data.forEach(skill => {
+      const dt = document.createElement('dt');
+      dt.classList.add('skill-item');
+      dt.textContent = skill.title;
+      dt.style.backgroundImage = `url(img/${skill.icon})`;
+
+      const dd = document.createElement('dd');
+      dd.classList.add('skill-level');
+
+      const div = document.createElement('div');
+      div.textContent = `${skill.level}%`;
+      div.style.width = `${skill.level}%`;
+
+      dd.append(div);
+      parentElement.append(dt, dd);
+    });
+  },
+
+  getComparer(prop) {
+    return (a, b) => {
+      if (a[prop] < b[prop]) return -1;
+      if (a[prop] > b[prop]) return 1;
+      return 0;
+    };
+  },
+
+  sortList(type) {
+    const { sortConfig } = this;
+
+    if (sortConfig.type === type) {
+      sortConfig.order = sortConfig.order === 'asc' ? 'desc' : 'asc';
+    } else {
+      sortConfig.type = type;
+      sortConfig.order = 'asc';
+    }
+    this.data.sort(this.getComparer(type));
+    if (sortConfig.order === 'desc') {
+      this.data.reverse(); 
+    }
+
+    this.generateList(skillList);
+  },
+
+  getData(path) {
+    fetch(path)
+      .then(response => {
+        if (!response.ok) throw new Error('Сеть ответила с ошибкой');
+        return response.json();
+      })
+      .then(data => {
+        this.data = data;            
+        this.generateList(skillList); 
+      })
+      .catch(() => {
+        document.querySelector('.section-skills').style.display = 'none';
+      });
+  }
+};
+
+const skillList = document.querySelector('.skill-list');
+const sortBtnsBlock = document.querySelector('.skills-sort');
+const navBtn = document.querySelector('.nav-btn');
+const navMenu = document.querySelector('.main-nav');
+
+
+
+sortBtnsBlock.addEventListener('click', function(event) {
+  const btn = event.target.closest('button');
+  if (btn && btn.dataset.type) {
+    skills.sortList(btn.dataset.type);
+  }
+});
+
+function getNavBtnLabel() {
+  return navBtn.querySelector('.visually-hidden');
+}
+
+const menu = {
+  open() {
+    navMenu.classList.remove('main-nav_closed');
+    navBtn.classList.remove('nav-btn-open');
+    navBtn.classList.add('nav-btn-close');
+    getNavBtnLabel().textContent = 'Закрыть меню';
+    navBtn.setAttribute('aria-expanded', 'true');
+  },
+
+  close() {
+    navMenu.classList.add('main-nav_closed');
+    navBtn.classList.remove('nav-btn-close');
+    navBtn.classList.add('nav-btn-open');
+    getNavBtnLabel().textContent = 'Открыть меню';
+    navBtn.setAttribute('aria-expanded', 'false');
+  }
+};
+
+menu.close();
+
+navBtn.addEventListener('click', () => {
+  if (navBtn.classList.contains('nav-btn-open')) {
+    menu.open();
+  } else {
+    menu.close();
+  }
+});
+
+const themeSwitch = document.querySelector('#switch');
+
+themeSwitch.checked = document.body.classList.contains('dark-theme');
+
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+  document.body.classList.add('dark-theme');
+  themeSwitch.checked = true;
+} else {
+  document.body.classList.remove('dark-theme');
+  themeSwitch.checked = false;
+}
+
+
+themeSwitch.addEventListener('change', function() {
+  if (this.checked) {
+    document.body.classList.add('dark-theme');
+    localStorage.setItem('theme', 'dark'); 
+  } else {
+    document.body.classList.remove('dark-theme');
+    localStorage.setItem('theme', 'light');
+  }
+});
+
+skills.getData('db/skills.json'); 
